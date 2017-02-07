5023530cf836a06419d41e4b2b296a640d58c66c
lazaguru <lazaguru2013@gmail.com>
2017-02-07 12:43:11 +0300

    Add files via upload

--- primetest.py
+++ primetest.py
@@ -1,32 +1,39 @@
 import unittest
-from Prime import *
+from primenumbergen import is_prime
 
-
-class PrimeTest(unittest.TestCase):
+class PrimesTest(unittest.TestCase):
 	def setUp(self):
-		self.prime = Prime()
+        self.prime = is_Prime()
+    """Tests for `primenumgen.py`."""
+    def test_is_four_non_prime(self):
+    """Is four correctly determined not to be prime?"""
+        self.assertFalse(is_prime(4), msg='Four is not prime!')
+    def test_is_zero_not_prime(self):
+    """Is zero correctly determined not to be prime?"""
+        self.assertFalse(is_prime(0))
+    def test_input_zero(self):
+        self.assertRaises(Exception):
+            get_primes(0)
 
-	def test_number_less_than_zero_not_prime(self):
-		self.assertFalse(self.prime.isPrime(-1))
+    def test_input_one(self):
+        self.assertRaises(Exception):
+            get_primes(1)
 
-	def test_1_is_not_prime(self):
-		self.assertFalse(self.prime.isPrime(1))
+    def test_list_is_not_greater_than_n(self):
+        list_returned = get_primes(100)
+        self.assertTrue(len(list_returned) < 100)
+    def test_number_less_than_zero_not_prime(self):
+        self.assertFalse(self.prime.isPrime(-1)) 
+    def test_1_is_not_prime(self):
+        self.assertFalse(self.prime.isPrime(1))
+    def test_5_is_prime(self):
+        self.assertTrue(self.prime.isPrime(5)) 
+    def test_massive_number_is_prime(self):
+        self.assertTrue(self.prime.isPrime(1066340417491710595814572169))
 
-	def test_2_is_prime(self):
-		self.assertTrue(self.prime.isPrime(2))
-
-	def test_4_is_not_prime(self):
-		self.assertFalse(self.prime.isPrime(4))
-
-	def test_5_is_prime(self):
-		self.assertTrue(self.prime.isPrime(5))
-
-	def test_massive_number_is_prime(self):
-		self.assertTrue(self.prime.isPrime(1066340417491710595814572169))
-
-	def test_massive_even_number_is_not_prime(self):
-		self.assertFalse(self.prime.isPrime(1066340417491710595814572168))
+    def test_massive_even_number_is_not_prime(self):
+        self.assertFalse(self.prime.isPrime(1066340417491710595814572168))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()              
